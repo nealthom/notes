8c592cbf597f67b4583f2efa729f67555ee4e1de
nealthom <nealthom@gmail.com>
2018-07-16 05:05:11 -0500

    Add files via upload

---
+++ scripts/notes-edit.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
+const dateElement = document.querySelector('#last-edited')
+let noteId = location.hash.slice(1)
+let notes = getSavedNotes()
+
+
+let note = notes.find((note)=>{
+    return note.id === noteId
+})
+
+
+
+if(!note){
+    location.assign('/index.html')
+}
+
+titleElement.value = note.title
+bodyElement.value = note.body
+dateElement.textContent = generateLastEdited(note.updatedAt)
+
+
+
+
+titleElement.addEventListener('input',(e)=>{
+   note.title = e.target.value
+   note.updatedAt = moment().valueOf()
+    dateElement.textContent = generateLastEdited(note.updatedAt)
+   saveNotes(notes)
+
+})
+
+bodyElement.addEventListener('input', (e) => {
+    note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    dateElement.textContent = generateLastEdited(note.updatedAt)
+    saveNotes(notes)
+
+})
+
+removeElement.addEventListener('click', (e) => {
+    removeNote(noteId)
+    saveNotes(notes)
+    location.assign('/')
+
+})
+
+window.addEventListener('storage',(e) =>{
+    if(e.key === 'notes'){
+            notes = JSON.parse(e.newValue)
+            note = notes.find((note) => {
+            return note.id === noteId
+        })
+
+        if (!note) {
+            location.assign('/index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+        dateElement.textContent = generateLastEdited(note.updatedAt)
+    }
+})
